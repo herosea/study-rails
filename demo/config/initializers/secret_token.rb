1c94142eddace545ec4fdf7cc66fb46b57df91d6
周雄海 <herosea.zhou@gmail.com>
2013-03-31 05:07:30 -0400

    初始化

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '3b0b18110843e66ab1a0f4f06ffa44b714bf57723a8b8329a8bd7b42bc39fbf6134ee50326159ca6d78a67f0f131dab5ff9ec206250db225eb186ce4358fa154'
